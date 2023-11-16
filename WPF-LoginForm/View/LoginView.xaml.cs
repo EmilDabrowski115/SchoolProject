9ca9da3b0bc5293ac7327e0058bafd03f3a17027
Emil <kingemil115@gmail.com>
2023-11-16 08:40:34 +0100

    Add project files.

---
+++ WPF-LoginForm/View/LoginView.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Navigation;
+
+namespace WPF_LoginForm.View
+{
+    /// <summary>
+    /// Interaction logic for LoginView.xaml
+    /// </summary>
+
+    public partial class LoginView : Window
+    {
+        public LoginView()
+        {
+            InitializeComponent();
+        }
+
+        private void RegisterButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Navigate to the registration view
+            RegisterView registerView = new RegisterView();
+            registerView.Show();
+
+            // Close the current window (optional)
+            this.Close();
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+                DragMove();
+        }
+
+        private void btnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            // Add your login logic here
+        }
+
+        private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            // Get the target URI from the Hyperlink
+            string targetUri = e.Uri.OriginalString;
+
+            // Create an instance of the target window and show it
+            Window targetWindow = (Window)Activator.CreateInstance(Type.GetType(targetUri));
+            targetWindow.Show();
+
+            // Close the current window (optional)
+            this.Close();
+        }
+    }
+}
